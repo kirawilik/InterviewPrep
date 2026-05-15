aae4cc43e86061ab14e2b6d2ac4d0fe7f420a299
kirawilik <khadijakchini@gmail.com>
2026-05-15 10:22:35 +0100

    add policies

---
+++ app/Policies/ConceptPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Concept;
+use App\Models\User;
+
+class ConceptPolicy
+{
+    public function view(User $user, Concept $concept): bool
+    {
+        return $user->id === $concept->domain->user_id;
+    }
+
+    public function update(User $user, Concept $concept): bool
+    {
+        return $user->id === $concept->domain->user_id;
+    }
+
+    public function delete(User $user, Concept $concept): bool
+    {
+        return $user->id === $concept->domain->user_id;
+    }
+
+    public function restore(User $user, Concept $concept): bool
+    {
+        return $user->id === $concept->domain->user_id;
+    }
+
+    public function forceDelete(User $user, Concept $concept): bool
+    {
+        return $user->id === $concept->domain->user_id;
+    }
+}
